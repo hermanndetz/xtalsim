2c5ba9253fe44006bd8b6ae492fbccb157ed4f98
Hermann Detz <hermann.detz@gmail.com>
2018-07-27 11:57:20 +0200

    Corrected license information

--- src/app/InterfaceGenerator.cpp
+++ src/app/InterfaceGenerator.cpp
@@ -1,3 +1,10 @@
+/**
+Copyright (C) 2018 Hermann Detz and Juergen Maier
+
+This software may be modified and distributed under the terms
+of the MIT license.  See the LICENSE file for details.
+*/
+
 
 /*******************************************************************************
 
